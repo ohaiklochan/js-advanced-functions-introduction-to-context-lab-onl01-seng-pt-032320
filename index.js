e75cad09d3c64a186c8d8e9847cc29a4d7deeb6b
Chloe Steffens <caforonda103@yahoo.com>
2020-10-26 01:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-function createEmployeeRecord(array) 
+function createEmployeeRecord(array) {
+  let record
+  return record = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
